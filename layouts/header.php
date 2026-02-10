e8284ba074ca8f453b9b54cde0e31afc92fa9193
NotAlex575 <dapontealessandro89gmail.com>
2026-02-10 11:44:32 +0100

    reso logo più grande e aggiunto distacco tra header e main

--- layouts/header.php
+++ layouts/header.php
@@ -1,7 +1,7 @@
 <header>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary mb-5">
         <div class="container d-flex align-items-center">
-            <img src="./img/Logo.png" alt="Logo" height="40">
+            <img src="./img/Logo.png" alt="Logo" height="70">
             <ul class="mb-0 ms-2">Header</ul>
         </div>
     </nav>
