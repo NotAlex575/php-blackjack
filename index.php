54f8d9680eda195283640b49327d79482f0c1bf2
NotAlex575 <dapontealessandro89gmail.com>
2026-02-10 11:08:19 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Blackjack575</title>
+</head>
+<body>
+    <?php 
+        include_once "./layouts/header.php";
+    ?>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="border">
+                    <h2 class="text-center">Blackjack575</h2>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php
+        include_once "./layouts/footer.php";
+    ?>
+</body>
+</html>
