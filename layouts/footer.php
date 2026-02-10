cbe5f76cbe3fe93eda430ebf2eec2282178bbbe1
NotAlex575 <dapontealessandro89gmail.com>
2026-02-10 11:12:25 +0100

    create suddivisioni header / main / footer

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,3 +1,7 @@
 <footer>
-    
+    <div class="container mt-4">
+        <div class="row">
+            <p class="text-center">footer</p>
+        </div>
+    </div>
 </footer>
--- layouts/header.php
+++ layouts/header.php
@@ -1,3 +1,7 @@
 <header>
-
+    <div class="container">
+        <div class="row">
+            <p class="text-center">Header</p>
+        </div>
+    </div>
 </header>
